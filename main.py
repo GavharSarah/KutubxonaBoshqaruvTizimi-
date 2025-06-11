c47cbc9c985fa9812cb6c433ab15bd06c5c72ea0
Gavhar <saraah5778@gmail.com>
2025-06-11 09:38:45 -0400

    login menu

--- main.py
+++ main.py
@@ -5,21 +5,31 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import crud
 
 
-def main():
-    print("=== Kutubxona Boshqaruv Tizimi ===")
-    while True:
-        role = input("\nKirish turi (1-Admin, 2-Foydalanuvchi, 0-Chiqish): ")
 
-        if role == "1":
+def login_menu():
+    print("=== Kutubxona Tizimiga Xush kelibsiz ===")
+    print("1. Admin sifatida kirish")
+    print("2. Foydalanuvchi sifatida kirish")
+    choice = input("Tanlang (1/2): ")
+
+    if choice == '1':
+        email = input("Admin email: ").strip()
+        password = input("Parol: ").strip()
+       
+        if email == "admin@kutubxona.uz" and password == "admin123":
+            print("Admin sifatida tizimga kirdingiz.")
             crud.admin_menu()
-        elif role == "2":
-            crud.user_menu()
-        elif role == "0":
-            print("Chiqildi.")
-            break
         else:
-            print("Notogri tanlov.")
+            print("Xato! Email yoki parol noto‘g‘ri.")
+    elif choice == '2':
+        name = input("Ismingizni kiriting: ").strip()
+        print(f"Xush kelibsiz, {name}!")
+        crud.user_menu()
+    else:
+        print("Notogri tanlov.")
 
 if __name__ == "__main__":
-    main()
+    login_menu()
+
+
 
